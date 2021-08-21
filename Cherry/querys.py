c1825333d78e0e47450b58186eedc4c9a853cc20
melika <melika.akz97@gmail.com>
2021-08-22 01:01:38 +0430

    the query is finished...!

--- Cherry/querys.py
+++ Cherry/querys.py
@@ -1,10 +1,49 @@
+from elasticsearch_dsl.query import Q
+from .documents import HomeDocument
 
-from .serializers import HomeDocument
-from elasticsearch_dsl import Q
 
-# query building
+def query_builder(must_list, should_list):
+    search = HomeDocument.search()
+    mustList = []
+    shouldList = []
+
+    # make must list query
+    for must in must_list:
+        mustList.append(Q('match', **{must[0]: must[1]}))
+    
+    # make should list query
+    for should in should_list:
+        if should[0] == 'rooms' or should[0] == 'livingArea' or should[0] == 'plotArea' or should[0] == 'constructionYear' :
+            mustList.append(Q("range", **{should[0]:{'gte': should[1]}}))
+
+        elif should[0] == 'price':
+            shouldList.append(Q("range", price={'lte': should[1]}))
+
+        else:
+            shouldList.append(Q('match', **{should[0]: should[1]}))
+    
+    q = Q('bool', must=mustList, should=shouldList)
+    
+    return search.query(q)
+
+
+# separator data(list) to must list, should list, could list
+def separator_data(query_list):
+    should_list, must_list = [],[]
+    for query in query_list:
+        if query[1] == 'M':
+            must_list.append([query[0],query[2]])
+
+        elif query[1] == 'S' or query[1] == 'should':
+            should_list.append([query[0],query[2]])
+
+    return query_builder(must_list, should_list, )
+
+
+# filter multy data
 def filter_data(find_filter, serializers): 
     search = HomeDocument.search()
+    query_list = []
     for obj in find_filter:
             
             if obj == 'id' and find_filter[obj] != "":
@@ -12,159 +51,93 @@ def filter_data(find_filter, serializers):
                 search = search.query("match", id= filter)
 
                 return search
-
-            # filter by street
+                
             elif obj == 'place.address.street' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter('match',place__address__street=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q('match',place__address__street=value[2:]) | ~Q('match',place__address__street=value[2:] ))
-
-                    elif value[0] == 'C':
-                        search = search.filter(Q('match',place__address__street=value[2:]) | Q('match',place__address__street="Schotse Hoolglanden" ))
-
-            # filter by city
+                if value.find(':'):
+                    query_list.append(['place__address__street', value[0], value[2:]])
+                    
             elif obj == 'place.address.city' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", place__address__city=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", place__address__city=value[2:])|~Q("match", place__address__city=value[2:]))
-
-            # filter by country
+                if value.find(':'):
+                    query_list.append(['place__address__city', value[0], value[2:]])
+                    
             elif obj == 'place.address.country' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", place__address__country=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", place__address__country=value[2:]) | ~Q("match", place__address__country=value[2:]))
-
-            # filter by zipcode
+                
+                if value.find(':'):
+                    query_list.append(['place__address__country', value[0], value[2:]])
+              
             elif obj == 'place.address.zipcode' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", place__address__zipcode=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", place__address__zipcode=value[2:]) | ~Q("match", place__address__zipcode=value[2:]))
-
-            # filter by houseNumber
+                if value.find(':'):
+                    query_list.append(['place__address__zipcode', value[0], value[2:]])
+                    
             elif obj == 'place.address.houseNumber' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", place__address__houseNumber=value[2:])
+                if value.find(':'):
+                    query_list.append(['place__address__houseNumber', value[0], value[2:]])
 
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", place__address__houseNumber=value[2:]) | ~Q("match", place__address__houseNumber=value[2:]))
-
-            # filter by lat
             elif obj == 'place.geolocation.lat' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", place__geolocation__lat=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", place__geolocation__lat=value[2:]) | ~Q("match", place__geolocation__lat=value[2:]))
-
-            # filter by lng
+                if value.find(':'):
+                    query_list.append(['place__geolocation__lat', value[0], value[2:]])
+                    
             elif obj == 'place.geolocation.lng' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", place__geolocation__lng=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", place__geolocation__lng=value[2:]) | ~Q("match", place__geolocation__lng=value[2:]))
-
-            # filter by price
+                if value.find(':'):
+                    query_list.append(['place__geolocation__lng', value[0], value[2:]])
+                    
             elif obj == 'price' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
-                search = search.query("range", price={'lte': value})
-
-            # filter by environment
+                query_list.append(['price', 'should', value])
+         
             elif obj == 'environment' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", environment=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", environment=value[2:]) | ~Q("match", environment=value[2:]))
-
-            # filter by rooms
+                if value.find(':'):
+                    query_list.append(['environment', value[0], value[2:]])
+                
             elif obj == 'rooms' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
-                search = search.query("range", rooms={'gte': value})
-
-            # filter by livingArea
+                query_list.append(['rooms', 'should', int(value)])
+                    
             elif obj == 'livingArea' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
-                search = search.query("range", livingArea={'gte': value})
+                query_list.append(['livingArea', 'should', value])
             
-            #filter by plotArea
             elif obj == 'plotArea' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
-                search = search.query("range", plotArea={'gte': value})
-            
-            # filter by kindOfHouse
+                query_list.append(['plotArea', 'should', value])
+                    
             if obj == 'kindOfHouse' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", kindOfHouse=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", kindOfHouse=value[2:]) | ~Q("match", kindOfHouse=value[2:]))
-
-            # filter by energyLabel
+                if value.find(':'):
+                    query_list.append(['kindOfHouse', value[0], value[2:]])
+                    
             elif obj == 'energyLabel' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", energyLabel=value[2:])
-
-                    elif value[0] == 'S':
-                        search = search.filter(Q("match", energyLabel=value[2:]) | ~Q("match", energyLabel=value[2:]))
-
-            # filter by constructionYear
+                if value.find(':'):
+                    query_list.append(['energyLabel', value[0], value[2:]])
+                    
             elif obj == 'constructionYear' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
-                search = search.filter(Q("range", constructionYear={'lte': value}))
-
-            # filter by suitableFor  
+                query_list.append(['constructionYear', 'should', value])
+                    
+                
             elif obj == 'suitableFor' and find_filter[obj] != "":
                 value = str(serializers.context.get('result')[str(obj)])
                 
-                if value.find(':') == 1:
-                    if value[0] == 'M':
-                        search = search.filter("match", suitableFor=value[2:])
-
-                    elif value[0] == 'S':
-                        if value[2:] == 'Single':
-                            search = search.filter(Q("match", suitableFor=value[2:]) | Q("match", suitableFor="Couple"))
-
-                        elif value[2:] == 'Couple':
-                            search = search.filter(Q("match", suitableFor=value[2:]) | Q("match", suitableFor="Family"))
-                        
-                        search = search.filter(Q("match", suitableFor=value[2:]) | ~Q("match", suitableFor="Family"))
-                        
+                if value.find(':'):
+                    query_list.append(['suitableFor', value[0], value[2:]])
+                    
+    search = separator_data(query_list)
     return search
