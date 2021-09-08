2945c34ebca58d355b5d68f2b6c8f6f325a61710
Melik97 <melika.az97@gmail.com>
2021-09-08 22:17:55 +0430

    ok

--- Cherry/documents.py
+++ Cherry/documents.py
@@ -6,11 +6,12 @@ def client_elasticsearch(index_name):
     es = Elasticsearch(host="localhost", port=9200)
     es = Elasticsearch("http://elastic:changeme@localhost:9200")
     search = es.search(index=index_name)
-    if index_name == 'cherry':
+
+    if index_name == 'cherry':#decision_model
         search = Search(index=index_name)
         return search
         
-    elif index_name == 'realstate':
+    elif index_name == 'real_estate': #50
         search = Search(index=index_name)
         return search
     
